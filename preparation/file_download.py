3f1a2f57ae34211d08ea41b77544dbbba0a94515
Kiryha <kiryhaaa13@gmail.com>
2019-02-07 21:42:03 +0200

    Added script to download files from urls

---
+++ preparation/file_download.py
@@ -0,0 +1,16 @@
+import requests
+#import urllib.request
+
+print('Downloading file using requests')
+
+url = 'https://ransomwaretracker.abuse.ch/downloads/RW_URLBL.txt'
+r = requests.get(url)
+
+with open('/Users/kirillkarpenok/files/practise/malicious-urls-detection/data/urls.csv', 'wb') as f:
+    f.write(r.content)
+
+# Retrieve HTTP meta-data
+print(r.status_code)
+print(r.headers['content-type'])
+print(r.encoding)
+
